8ea8b69a890e415860c6d7c91b1b41cc426700bf
AbdelouhhabCHARBAK <86660838+PORTRAITART1@users.noreply.github.com>
2026-04-23 17:50:00 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ import envManager from "./config/envManager.js";
 
 const app = express();
 const PORT = envManager.get('port', 3000);
-const PI_API_KEY = process.env.PI_API_KEY;7a54rpa8aelvqsoiu9m23ddingkbbqlcsctkp7afzoymylxtw6ob9vhkwppov0is
+const PI_API_KEY = process.env.PI_API_KEY;const PI_API_KEY = process.env.PI_API_KEY;
 
 // Log startup info
 logger.info(`\n${'='.repeat(60)}`);
